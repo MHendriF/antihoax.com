1751b81c13b1a583aad4ed856fe2d31adeccbd84
leopard28 <muhamad.hendri.febriansyah14@mhs.if.its.ac.id>
2017-04-20 05:32:02 +0700

    coba

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\User;
+use Validator;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
@@ -35,5 +37,14 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest', ['except' => 'logout']);
+    }
+
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name'      => 'required',
+            'username'       => 'required',
+            'password'  => 'required|confirmed',
+        ]);
     }
 }
