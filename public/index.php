e712e19fa46fc22591805d338bafb0c9c7a59607
leopard28 <muhamad.hendri.febriansyah14@mhs.if.its.ac.id>
2017-04-19 23:39:00 +0700

    synchronize to laravel

--- public/index.php
+++ public/index.php
@@ -1,228 +1,58 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
+ */
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| our application. We just need to utilize it! We'll simply require it
+| into the script here so that we don't have to worry about manual
+| loading any of our classes later on. It feels great to relax.
+|
+*/
 
-    <title>ANTI-HOAX!</title>
+require __DIR__.'/../bootstrap/autoload.php';
 
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+/*
+|--------------------------------------------------------------------------
+| Turn On The Lights
+|--------------------------------------------------------------------------
+|
+| We need to illuminate PHP development, so let us turn on the lights.
+| This bootstraps the framework and gets it ready for use, then it
+| will load up this application so that we can run it and send
+| the responses back to the browser and delight our users.
+|
+*/
 
-    <!-- Custom CSS -->
-    <link href="css/agency.css" rel="stylesheet">
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
-    <!-- Custom Fonts -->
-    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request
+| through the kernel, and send the associated response back to
+| the client's browser allowing them to enjoy the creative
+| and wonderful application we have prepared for them.
+|
+*/
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-</head>
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
 
-<body id="page-top" class="index">
-    <!-- Navigation -->
-    <nav class="navbar navbar-default navbar-fixed-top">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header page-scroll">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <a class="navbar-brand page-scroll" href="#page-top">HOME</a>
-                </button>
-            </div>
+$response->send();
 
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                    <li class="hidden">
-                        <a href="#page-top"> HOME</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#detect">Hoax Detector</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#reliable_site">Reliable Site</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#example">Example of Hoax</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#recent">Recent Report</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#tips">How to Detect Hoax</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#about">About Us</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container-fluid -->
-    </nav>
-
-    <!-- Header -->
-    <header>
-        <div class="container">
-            <div class="intro-text">
-                <div class="intro-lead-in">Welcome To Anti Hoax Website</div>
-                <div class="intro-heading">Let's broke the Hoax!</div>
-            </div>
-        </div>
-    </header>
-
-    <!-- dETECT Section -->
-    <section id="detect">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h2 class="section-heading">Detect Hoax</h2>
-                </div>
-            </div>
-            <div class="row text-center">
-                    <h4 class="text-muted">Insert the link below</h4>
-                    <input>
-                    <button>DETECT</button>
-            </div>
-        </div>
-    </section>
-
-    <!-- Reliable Section -->
-    <section id="reliable"  class="bg-light-gray">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h2 class="section-heading">List of Reliable Website</h2>
-                </div>
-            </div>
-            <div class="row text-center">
-                    <h4 class="text-muted">Insert the link below</h4>
-                    <input>
-                    <button>DETECT</button>
-            </div>
-        </div>
-    </section>
-    
-    <!-- Reliable Section -->
-    <section id="reliable_site">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h2 class="section-heading">List of Reliable Website</h2>
-                </div>
-            </div>
-        </div>
-    </section>
-    
-    <!-- Reliable Section -->
-    <section id="example"  class="bg-light-gray">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h2 class="section-heading">Example of Hoax</h2>
-                </div>
-            </div>
-        </div>
-    </section>
-    
-    <!-- Reliable Section -->
-    <section id="recent">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h2 class="section-heading">Recent Report of Hoax</h2>
-                </div>
-            </div>
-        </div>
-    </section>
-    
-    <!-- Reliable Section -->
-    <section id="tips"  class="bg-light-gray">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h2 class="section-heading">How to detect Hoax</h2>
-                </div>
-            </div>
-            
-        </div>
-    </section>
-    
-    <!-- Reliable Section -->
-    <section id="about">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h2 class="section-heading">About Us</h2>
-                </div>
-            </div>
-            
-        </div>
-    </section>
-    
-    <footer>
-        <div class="container" class="bg-light-gray">
-            <div class="row">
-                <div class="col-md-4">
-                    <span class="copyright">Copyright &copy; SI UPT FASOR 2016</span>
-                </div>
-                <div class="col-md-4">
-                    <ul class="list-inline social-buttons">
-                        <li><a href="#"><i class="fa fa-twitter"></i></a>
-                        </li>
-                        <li><a href="#"><i class="fa fa-facebook"></i></a>
-                        </li>
-                        <li><a href="#"><i class="fa fa-linkedin"></i></a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-md-4">
-                    <ul class="list-inline quicklinks">
-                        <li><a href="#">Privacy Policy</a>
-                        </li>
-                        <li><a href="#">Terms of Use</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </footer>
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-    <!-- Plugin JavaScript -->
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
-    <script src="js/classie.js"></script>
-    <script src="js/cbpAnimatedHeader.js"></script>
-
-    <!-- Contact Form JavaScript -->
-    <script src="js/jqBootstrapValidation.js"></script>
-    <script src="js/contact_me.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="js/agency.js"></script>
-
-</body>
-
-</html>
+$kernel->terminate($request, $response);
---
+++ resources/views/backend/layouts/partials/scripts_auth.blade.php
@@ -0,0 +1,6 @@
+<!-- jQuery 2.2.3 -->
+<script src="{{ asset('/backend/plugins/jQuery/jquery-2.2.3.min.js') }}"></script>
+<!-- Bootstrap 3.3.6 -->
+<script src="{{ asset('/backend/bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>
+<!-- iCheck -->
+<script src="{{ asset('/backend/plugins/iCheck/icheck.min.js') }}" type="text/javascript"></script>
