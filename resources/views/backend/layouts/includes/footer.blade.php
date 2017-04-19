d380ddc33f34e907d37e468e97ba4993623bd768
leopard28 <muhamad.hendri.febriansyah14@mhs.if.its.ac.id>
2017-04-20 05:54:01 +0700

    home admin update

---
+++ resources/views/backend/layouts/includes/footer.blade.php
@@ -0,0 +1,6 @@
+<!-- Main Footer -->
+<footer class="main-footer">
+   
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2016 <a href="http://#">IMK</a>.</strong> Created by <a href="http://#">ADMIN</a>.
+</footer>
